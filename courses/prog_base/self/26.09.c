2bb92cd54e78d47f6dcb999f5c00f04ce6e74cf7
gribo4eg <gribo4eg98@gmail.com>
2015-09-26 15:48:43 +0300

    s

---
+++ courses/prog_base/self/26.09.c
@@ -0,0 +1,20 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int h, m, code;
+float g;
+printf("vvedit: hours, minutes ta kod mista \n");
+scanf("%i %i %i", &h, &m, &code);
+switch(code){
+case 44: g=((h*60)+m)*0.44; break;
+case 66: g=((h*60)+m)*0; break;
+case 1 : g=((h*60)+m)*30; break;
+default: {
+g=((h*60)+m)*1;
+return 1;}
+}
+printf("vartist: %f", g);
+return 0;
+}

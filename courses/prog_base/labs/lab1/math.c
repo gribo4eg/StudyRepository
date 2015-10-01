dc1c2ab3e6765380f9e93aa2f094c30fe9cdc822
gribo4eg <gribo4eg98@gmail.com>
2015-10-01 19:46:46 +0300

    math

---
+++ courses/prog_base/labs/lab1/math.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double calc(double x, double y, double z) {
+double a;
+
+int main(){
+float a0, a1, a2;
+a0 = (pow(x, (y+1)))/(pow((x-y), (1/z)));
+a1 = y/(4*(fabs(x + y)));
+a2 = pow(x, (1/fabs(sin(y))));
+a = a0 + a1 + a2;
+printf("a= %d", a);
+return 0;
+}
+return a;
+}
+

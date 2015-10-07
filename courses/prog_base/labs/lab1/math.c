62b1b263000a0f1cfaf54181fdcc6341e4ded7fe
gribo4eg <gribo4eg98@gmail.com>
2015-10-07 21:25:18 +0300

    Update math.c

--- courses/prog_base/labs/lab1/math.c
+++ courses/prog_base/labs/lab1/math.c
@@ -7,21 +7,12 @@
 double calc(double x, double y, double z) {
 double a;
 double a0, a1, a2;
-a0 = (pow(x, (y+1)))/(pow((x-y), (1/z)));
-a1 = y/(4*(fabs(x + y)));
-a2 = pow(x, (1/fabs(sin(y))));
+a0 = ((pow(x, y+1)) / (pow((x - y), 1/z)));
+a1 = y / (4 * fabs(x + y));
+a2 = pow(x, (1/(fabs(sin(y)))));
 a = a0 + a1 + a2;
 if (z==0, x==y, x==0, y==0){
     return NAN;
 }
 return a;
 }
-/*
-int main() {
-
-double a = calc(2.242, 1.234, 0.234);
-printf("a= %d", a);
-
-}
-
-*/

bfc0a6de3664e8d128a3455af68878ce483964f2
gribo4eg <gribo4eg98@gmail.com>
2015-10-08 21:06:48 +0300

    Update condition.c

--- courses/prog_base/labs/lab1/condition.c
+++ courses/prog_base/labs/lab1/condition.c
@@ -1,125 +1,90 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <math.h>
 
-int satisfies(int a, int b, int c) {
-int modmin, sum2, maximum, minimum;
-if (a<0, b<0, c<0) {
-    if (abs(a) < abs(b), abs(a) < abs(c)){
-        modmin = abs(a);
-        sum2 = b + c;
-    }
-    if (abs(b) < abs(a), abs(b) < abs(c)){
-        modmin = abs(b);
-        sum2 = a + c;
-    }
-    if (abs(c) < abs(a), abs(c) < abs(b)){
-        modmin = abs(c);
-        sum2 = a + b;
-    }
-    if (sum2 < -256, modmin < 256, ((int)log2(modmin))%1 == 0){
-            printf("1");
-            return 0;
+int satisfies(int a, int b, int c){
+int sum2, modmin, mx, mn;
+if (a<0, b<0, c<0){
+        if (a<b, a<b){
+            modmin=abs(a);
+            sum2=b+c;
+        }
+        if (b<a,b<c){
+            modmin=abs(b);
+            sum2=a+c;
+        }
+        if (c<a,c<b){
+            modmin=abs(c);
+            sum2=a+b;
+        }
+        if (sum2<-256, (int)log2(modmin)%1==0, modmin<256){
+            return 1;
+        }
+} else {
+if (abs(sum2)>modmin, sum2>-256){
+    return 1;
+}
+}
+if (a<0, b>=0, c>=0){
+    if (a>-256){
+        return 1;
     }
     else {
-        if (abs(sum2) > modmin, sum2 > 256);{
-            printf("1");
-            return 0;
-        }
+        return 0;
     }
+}
+if (b<0, a>=0, c>=0){
+    if (b>-256){
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+if (c<0, b>=0, a>=0){
+    if (c>-256){
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+if (a<0, b<0, c>=0){
+    if (3*(a+b)>-256){
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+if (a<0, c<0, b>=0){
+    if (3*(a+c)>-256){
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+if (c<0, b<0, a>=0){
+    if (3*(c+b)>-256){
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+if (a>=0, b>=0, c>=0){
+    mx = fmax(fmax(a,b), fmax(b,c));
+    mn = fmin(fmin(a,b), fmin(b,c));
+    if (mn==0){
+        return 0;
+    }
+    if (mx==0, mn!=0){
+        return 1;
+    }
+    if (mx%mn==0){
+        return 1;
+    }
+}
+}
 
-}
-if (a < 0, b >= 0, c >= 0){
-    if (a > -256){
-        printf("1");
-        return 0;
-    }
-    else {
-        printf("0");
-        return 0;
-    }
-}
-if (b < 0, a >= 0, c >=0){
-    if (b > -256){
-        printf("1");
-        return 0;
-    }
-    else {
-        printf("0");
-        return 0;
-    }
-}
-if (c < 0, a >= 0, b >= 0){
-    if (c > -256){
-        printf("1");
-        return 0;
-    }
-    else {
-        printf("0");
-        return 0;
-    }
-}
-if (a < 0, b < 0, c >= 0){
-    if ((a + b)*3 > -256){
-        printf("1");
-        return 0;
-    }
-    else {
-        printf("0");
-        return 0;
-    }
-}
-if (a < 0, b >= 0, c < 0){
-    if ((a + c)*3 > -256){
-        printf("1");
-        return 0;
-    }
-    else {
-        printf("0");
-        return 0;
-    }
-}
-if (a >= 0, b < 0, c < 0){
-    if ((c + b)*3 > -256){
-        printf("1");
-        return 0;
-    }
-    else {
-        printf("0");
-        return 0;
-    }
-}
-if (a >= 0, b >= 0, c >= 0){
-    if (a > b, a > c){
-        maximum = a;
-    }
-    if (b > a, b > c){
-        maximum = b;
-    }
-    if (c > a, c > b){
-        maximum = c;
-    }
-    if (a < b, a < c){
-        minimum = a;
-    }
-    if (b < a, b < c){
-        minimum = b;
-    }
-    if (c < a, c < b){
-        minimum = c;
-    }
-    if (maximum%minimum == 0){
-        printf("1");
-    }
-    else {
-        if (minimum == 0){
-            printf("0");
-            return 0;
-        }
-        if (maximum == 0, minimum != 0){
-            printf("1");
-            return 0;
-        }
-    }
-}
-}

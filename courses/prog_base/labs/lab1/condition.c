2caf8b41273da22060528522bd182b597ba55c15
gribo4eg <gribo4eg98@gmail.com>
2015-10-09 22:20:34 +0300

    Update condition.c

--- courses/prog_base/labs/lab1/condition.c
+++ courses/prog_base/labs/lab1/condition.c
@@ -3,88 +3,109 @@
 #include <math.h>
 
 int satisfies(int a, int b, int c){
-int sum2, modmin, mx, mn;
-if (a<0, b<0, c<0){
-        if (a<b, a<b){
+int res, sum2, modmin, mx, mn;
+if (a<0 && b<0 && c<0){
+        if (a<b && a<c){
             modmin=abs(a);
             sum2=b+c;
-        }
-        if (b<a,b<c){
+        } else
+        if (b<a && b<c){
             modmin=abs(b);
             sum2=a+c;
-        }
-        if (c<a,c<b){
+        } else{
+        if (c<a && c<b){
             modmin=abs(c);
             sum2=a+b;
         }
-        if (sum2<-256, (int)log2(modmin)%1==0, modmin<256){
-            return 1;
         }
-} else {
-if (abs(sum2)>modmin, sum2>-256){
-    return 1;
+        if (sum2<-256 && (int)log2(modmin)%1==0 && modmin<256){
+            res= 1;
+        } else {
+if (abs(sum2)>modmin && sum2>-256){
+    res= 1;
 }
 }
-if (a<0, b>=0, c>=0){
+}
+if (a<0 && b>=0 && c>=0){
     if (a>-256){
-        return 1;
+        res= 1;
     }
     else {
-        return 0;
+        res= 0;
     }
 }
-if (b<0, a>=0, c>=0){
+if (b<0 && a>=0 && c>=0){
     if (b>-256){
-        return 1;
+        res= 1;
     }
     else {
-        return 0;
+    res= 0;
     }
 }
-if (c<0, b>=0, a>=0){
+if (c<0 && b>=0 && a>=0){
     if (c>-256){
-        return 1;
+        res= 1;
     }
     else {
-        return 0;
+        res= 0;
     }
 }
-if (a<0, b<0, c>=0){
+if (a<0 && b<0 && c>=0){
     if (3*(a+b)>-256){
-        return 1;
+        res= 1;
     }
     else {
-        return 0;
+        res= 0;
     }
 }
-if (a<0, c<0, b>=0){
+if (a<0 && c<0 && b>=0){
     if (3*(a+c)>-256){
-        return 1;
+        res= 1;
     }
     else {
-        return 0;
+        res= 0;
     }
 }
-if (c<0, b<0, a>=0){
+if (c<0 && b<0 && a>=0){
     if (3*(c+b)>-256){
-        return 1;
+        res= 1;
     }
     else {
-        return 0;
+        res= 0;
     }
 }
-if (a>=0, b>=0, c>=0){
-    mx = fmax(fmax(a,b), fmax(b,c));
-    mn = fmin(fmin(a,b), fmin(b,c));
+if (a>=0 && b>=0 && c>=0){
+   if (a>b && a>c){
+    mx=a;
+   } else
+   if (b>a && b>c){
+    mx=b;
+   } else {
+   if (c>a && c>b){
+    mx=c;
+   }
+   }
+   if (a<b && a<c){
+    mn=a;
+   } else
+   if (b<a && b<c){
+    mn=b;
+   } else {
+   if (c<a && c<b){
+    mn=c;
+   }
+   }
     if (mn==0){
-        return 0;
-    }
-    if (mx==0, mn!=0){
-        return 1;
-    }
+        res= 0;
+    } else
+    if (mx==0 && mn!=0){
+        res= 1;
+    } else
     if (mx%mn==0){
-        return 1;
+        res= 1;
+    } else {
+        res= 0;
     }
 }
+return res;
 }
-

a389cf6fbe6acd8cb8805740c2db3639707bd4c4
gribo4eg <gribo4eg98@gmail.com>
2016-04-17 22:07:30 +0300

    update

--- courses/prog_base_3/project/main.cpp
+++ courses/prog_base_3/project/main.cpp
@@ -1,61 +1,98 @@
 #include <SFML/Graphics.hpp>
 
+
+using namespace std;
 using namespace sf;
 
-int main()
+int main(void)
 {
-    RenderWindow window(VideoMode(500, 500), "Terra! demo");
+    RenderWindow window(VideoMode(1280, 720), "SFML works!");
 
-    Texture Hero;
-    Hero.loadFromFile("variant2.0.png");
+    float CurrentFrame = 0;
 
-    double currentPict = 0;
+    Clock clock;
 
-    Sprite sprite;
-    sprite.setTexture(Hero);
-    sprite.setTextureRect(IntRect(0, 58, 40, 34));
-    sprite.setPosition(50, 100);
+    Image Hero;
+    Hero.loadFromFile("images/mob.png");
+    Hero.createMaskFromColor(Color(0,0,0,0));
+
+    Texture HeroTexture;
+    HeroTexture.loadFromImage(Hero);
+
+    Sprite HeroSprite;
+    HeroSprite.setTexture(HeroTexture);// width.x = 115; width.y = 190
+    HeroSprite.setTextureRect(IntRect(0, 10, 115, 190));
+    HeroSprite.setPosition(900, 25);
+    HeroSprite.setScale(0.4,0.4);
 
     while (window.isOpen())
     {
-
         Event event;
+
+        float time = clock.getElapsedTime().asMicroseconds();
+        clock.restart();
+        time = time/500;
 
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed || Keyboard::isKeyPressed(Keyboard::Escape))
                 window.close();
         }
 
         if(Keyboard::isKeyPressed(Keyboard::Left))
         {
-                sprite.move(-0.1,   0);
+            CurrentFrame += 0.005 * time;
+            if(CurrentFrame > 6)
+                CurrentFrame -= 6;
+            HeroSprite.setTextureRect(IntRect(115 * int(CurrentFrame), 10, 115, 190));
+            HeroSprite.move(-0.1 * time, 0);
         }
 
         if(Keyboard::isKeyPressed(Keyboard::Right))
         {
-                sprite.move(0.1,   0);
-
-                currentPict += 0.09;
-                if(currentPict >3)
-                    currentPict -=3;
-                sprite.setTextureRect(IntRect(42*int(currentPict), 58, 40, 34));
-        }
-
-        if(Keyboard::isKeyPressed(Keyboard::Up))
-        {
-                sprite.move(0,  -0.1);
+            CurrentFrame += 0.005 * time;
+            if(CurrentFrame > 6)
+                CurrentFrame -= 6;
+            HeroSprite.setTextureRect(IntRect(115 * int(CurrentFrame), 210, 115, 190));
+            HeroSprite.move(0.1 * time, 0);
         }
 
         if(Keyboard::isKeyPressed(Keyboard::Down))
         {
-                sprite.move(0,   0.1);
+            HeroSprite.move(0, 0.1 * time);
+            HeroSprite.setTextureRect(IntRect(0, 10, 115, 190));
+        }
+
+        if(Keyboard::isKeyPressed(Keyboard::Up))
+        {
+            HeroSprite.move(0, -0.1 * time);
+            HeroSprite.setTextureRect(IntRect(0, 10, 115, 190));
         }
 
         window.clear(Color::White);
-        window.draw(sprite);
+        window.draw(HeroSprite);
         window.display();
     }
 
     return 0;
 }
+
+class Creatures
+{
+public:
+    creatures(string file, float x, float width,
+              float height);
+    float x, width, heigth, speed_x, speed;
+    int direction;
+    string imageName;
+    Image image;
+    Texture texture;
+    Sprite sprite;
+};
+
+Creatures::creatures(string file, float x, float width,
+              float height)
+              {
+                    imageName = file;
+
+              }

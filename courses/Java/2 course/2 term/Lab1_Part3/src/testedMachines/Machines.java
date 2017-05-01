7bdaeab7c654a43c191ab619da0f040296092ff3
Alexander <gribo4eg98@gmail.com>
2017-05-01 15:59:15 +0300

    start course 3

--- courses/Java/2 course/2 term/Lab1_Part3/src/testedMachines/Machines.java
+++
@@ -1,9 +0,0 @@
-package testedMachines;
-
-public class Machines {
-	
-	public static FSM fsm = new SwitchFSM();
-	//public static FSM fsm = new TransitionTable();
-	//public static FSM fsm = new StateFSM();
-	
-}

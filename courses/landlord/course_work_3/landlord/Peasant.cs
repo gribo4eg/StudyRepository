a793c78ee81f5e93ea46743df2a2b2d37d57333c
Alexander <gribo4eg98@gmail.com>
2017-05-02 03:12:11 +0300

    landlord progress = 0.000001% =D

---
+++ courses/landlord/course_work_3/landlord/Peasant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace landlord
+{
+    [DataContract]
+    public class Peasant
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public int Age { get; set; }
+
+        public Peasant(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ courses/landlord/course_work_3/landlord/ViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace landlord
+{
+    public class ViewModel
+    {
+
+        public List<Peasant> Peasants { get; private set; }
+
+        public ViewModel()
+        {
+            Peasants = new List<Peasant>();
+
+            using (Stream stream = new FileStream("peasants.json", FileMode.Open))
+            {
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(List<Peasant>));
+
+                Peasants = (List<Peasant>)ser.ReadObject(stream);
+            }
+        }
+    }
+}
+
+/*  
+ *  using (Stream stream = new FileStream("peasants.json", FileMode.Create))
+            {
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(List<Peasant>));
+
+                List<Peasant> peasants = new List<Peasant> {
+                    new Peasant("PeasantOne", 10),
+                    new Peasant("PeasantTwo", 20)
+                };
+
+                ser.WriteObject(stream, peasants);
+            }
+*/
